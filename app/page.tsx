447076723fb174139d62b490cc0214e5cd12665e
ducoday12345 <ducoday12345@gmail.com>
2025-05-16 03:09:33 -0400

    Add usePrivy import to enable authentication features in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,16 @@ import { SecuritySection } from "@/components/security-section";
 import { Footer } from "@/components/footer";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ParticleBackground } from "@/components/particle-background";
+import { usePrivy } from "@privy-io/react-auth";
 
 export default function Home() {
+  
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       <div className="min-h-screen flex flex-col relative overflow-hidden">
+
+        
+        
         <ParticleBackground />
         <Navbar />
         <main className="flex-1">
